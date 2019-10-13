31eb82fcd39b0a78e01a415003e1fc16dda3ca21
Minch <yeah@minch.me>
2019-10-13 22:28:31 +0800

    update Queue

--- Applications/Queue/start.php
+++ Applications/Queue/start.php
@@ -3,7 +3,7 @@
 // 自动加载类
 require_once dirname(__DIR__) . '/loader.php';
 
-\Beanbun\Queue\MemoryQueue::server();
+\Queue\Queue::server();
 
 // 如果不是在根目录启动，则运行runAll方法
 if(!defined('GLOBAL_START')){
