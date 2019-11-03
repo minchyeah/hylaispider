5cd1789e310aab1e2d8b08a7a91ce51aaa2a3c7d
Minch <yeah@minch.me>
2019-11-03 23:44:23 +0800

    update

--- Applications/Spider/Spider.php
+++ Applications/Spider/Spider.php
@@ -128,6 +128,8 @@ class Spider
 
         if ($this->afterDownloadPage) {
             $this->afterDownloadPageHooks[] = $this->afterDownloadPage;
+        }else{
+            $this->afterDownloadPageHooks[] = [$this, 'defaultAfterDownloadPage'];
         }
 
         if ($this->discoverUrl) {
@@ -334,6 +336,111 @@ class Spider
         } else {
             $this->error();
         }
+    }
+
+    public function defaultAfterDownloadPage()
+    {
+        if($this->urlType != 'content'){
+            return;
+        }
+        preg_match('/tid=(\d+)/', $this->url, $matches);
+        if(!isset($matches[1]) || !is_numeric($matches[1])){
+            return;
+        }
+        $db = Db::instance(\Config\Database::$default);
+        $data = array();
+        $data['tid'] = $matches[1];
+        $row = $db->select('id,tid')->from('pw_spider')->where('tid', $data['tid'])->row();
+        if(isset($row['id']) && $row['id'] > 0){
+            return;
+        }
+        $start_time = 0;
+        $end_time = 0;
+        $set_rows = $db->select('skey,svalue')
+                ->from('pw_spider_settings')
+                ->where('skey', 'start_time')
+                ->orWhere('skey', 'end_time')
+                ->query();
+        if(!is_array($set_rows)){
+            return;
+        }
+        foreach ($set_rows as $set) {
+            if($set['skey'] == 'start_time'){
+                $start_time = strtotime($set['svalue']);
+            }
+            if($set['skey'] == 'end_time'){
+                $end_time = strtotime($set['svalue']);
+            }
+        }
+        if($start_time ==0 || $end_time == 0){
+            return;
+        }
+        $data['url'] = $this->url;
+        $data['spide_time'] = time();
+        $html = $this->page;
+        $fields = \Config\Spider::$fields;
+        foreach ($fields as $conf)
+        {
+            // 当前field抽取到的内容是否是有多项
+            $repeated = isset($conf['repeated']) && $conf['repeated'] ? true : false;
+            // 当前field抽取到的内容是否必须有值
+            $required = isset($conf['required']) && $conf['required'] ? true : false;
+
+            if (empty($conf['name'])) 
+            {
+                break;
+            }
+            $values = NULL;
+            // 如果定义抽取规则
+            if (!empty($conf['selector'])) 
+            {
+                // 没有设置抽取规则的类型 或者 设置为 xpath
+                if (!isset($conf['selector_type']) || $conf['selector_type']=='xpath') 
+                {
+                    // 如果找不到，返回的是false
+                    $values = $this->get_fields_xpath($html, $conf['selector'], $conf['name']);
+                }
+                elseif ($conf['selector_type']=='css') 
+                {
+                    $values = $this->get_fields_css($html, $conf['selector'], $conf['name']);
+                }
+                elseif ($conf['selector_type']=='regex') 
+                {
+                    $values = $this->get_fields_regex($html, $conf['selector'], $conf['name']);
+                }
+            }
+            if(isset($conf['filter']) && $conf['filter'] && $values){
+                    $values = preg_replace($conf['filter'], '', $values);
+            }
+            //$this->log(print_r($values, true));
+            switch ($conf['name']) {
+                case 'author':
+                    $author_row = $db->select('*')
+                            ->from('pw_spider_authors')
+                            ->where('sp_author', $values)
+                            ->row();
+                    if(!isset($author_row['id'])){
+                        return;
+                    }
+                    break;
+                case 'post_time':
+                    $values = strtotime($values);
+                    if($values<$start_time || $values>$end_time){
+                        unset($data, $values);
+                        return;
+                    }
+                    break;
+                case 'content':
+                    $values = str_replace('&#13;', '<br >', $values);
+                    break;
+                
+                default:
+                    # code...
+                    break;
+            }
+            $data[$conf['name']] = $values;
+        }
+        $db->insert('pw_spider')->cols($data)->query();
     }
 
     public function defaultDiscoverUrl()
